edfee5596fb7268c305a39c39131bb6e426e90f2
Justice <sherllyj191@gmail.com>
2023-09-30 12:42:54 +0100

    Added Header.

--- src/widgets/components/Header/index.tsx
+++ src/widgets/components/Header/index.tsx
@@ -1,7 +1,28 @@
+import styled from "@emotion/styled";
+import { Logo } from "./components/Logo";
+
 export default function Header() {
   return (
-    <header>
-      <section>Header</section>
-    </header>
+    <HeaderElement>
+      <Logo />
+    </HeaderElement>
   );
 }
+
+const HeaderElement = styled.header`
+  position: sticky;
+  top: 0;
+  z-index: 5;
+  height: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  backdrop-filter: blur(8.5px);
+  transition: 0.4s ease-in-out;
+  /* perspective: 800px; */
+  border-bottom: 1px solid rgba(255, 255, 255, 0.1);
+
+  & > * {
+    /* border: 1px solid white; */
+  }
+`;
