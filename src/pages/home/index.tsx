5d5f289080e9bc63671fde65bbc52093c54be746
Justice <sherllyj191@gmail.com>
2023-10-01 04:22:31 +0100

    Implemented Redirect.

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,11 @@
+import { Navigate } from "react-router-dom";
+
 const Home = () => {
   return (
     <main>
       <section>Home</section>
+
+      <Navigate to="/launchpad" replace={true} />
     </main>
   );
 };
